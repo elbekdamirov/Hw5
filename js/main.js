595ea5c98d298b03020bd332fbdf467f6cb3a655
elbekdamirov <elbekmain@gmail.com>
2025-03-21 23:11:53 +0500

    1-task done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-doira = {
-    PI: 3.14,
-    
-}
+//---------------------------1---------------------------------//
+// doira = {
+//   PI: 3.14,
+//   //   getInfo() {
+//   //     console.log(`PI = ${this.PI}, Radius = ${this.radius}`);
+//   //   }, //info tekshirish uchun ozim yozib qoydim
+//   set addRadius(r) {
+//     this.radius = r;
+//   },
+//   findArea() {
+//     yuza = this.radius ** 2 * this.PI;
+//     console.log("Doira yuzasi = ", yuza);
+//   },
+// };
+
+// doira.addRadius = 5;
+// doira.findArea();
+
+
+//-------------------------2------------------------------------//
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+
+    this.radius = r;
+  },
+  findArea() {
+    yuza = this.radius ** 2 * this.PI;
+    console.log("Doira yuzasi = ", yuza);
+  },
+};
+
+doira.addRadius = 5;
+doira.findArea();
