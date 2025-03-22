b62b31a2a48899739b9e565cc8f59065247c72de
elbekdamirov <elbekmain@gmail.com>
2025-03-22 11:11:10 +0500

    All tasks done

--- js/main.js
+++ js/main.js
@@ -60,3 +60,61 @@
 // const data = { a: 3, b: 5, c: 3, d: 7, e: 5, f: 5 };
 
 // console.log(findMostFrequentValue(data));
+
+//------------------------4------------------------------------//
+// function flattenObject(obj, prefix = "") {
+//   let result = {};
+
+//   for (let key in obj) {
+//     if (typeof obj[key] === "object" && obj[key] !== null) {
+//       Object.assign(result, flattenObject(obj[key], prefix + key + "."));
+//     } else {
+//       result[prefix + key] = obj[key];
+//     }
+//   }
+
+//   return result;
+// }
+
+// const data = {
+//   user: {
+//     name: "Ali",
+//     address: {
+//       city: "Tashkent",
+//       zip: "100100",
+//     },
+//   },
+// };
+
+// console.log(flattenObject(data));
+
+//-----------------------5-------------------------------------//
+// function deepEqual(obj1, obj2) {
+//   if (obj1 === obj2) return true;
+//   if (
+//     typeof obj1 !== "object" ||
+//     typeof obj2 !== "object" ||
+//     obj1 === null ||
+//     obj2 === null
+//   ) {
+//     return false;
+//   }
+
+//   let keys1 = Object.keys(obj1);
+//   let keys2 = Object.keys(obj2);
+
+//   if (keys1.length !== keys2.length) return false;
+
+//   for (let key of keys1) {
+//     if (!deepEqual(obj1[key], obj2[key])) return false;
+//   }
+
+//   return true;
+// }
+
+// const obj1 = { a: 1, b: { c: 2, d: 3 } };
+// const obj2 = { a: 1, b: { c: 2, d: 3 } };
+// const obj3 = { a: 1, b: { c: 2, d: 4 } };
+
+// console.log(deepEqual(obj1, obj2));
+// console.log(deepEqual(obj1, obj3));
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,11 +1,27 @@
+  if (obj1 === obj2) return true;
+//   if (
+//     typeof obj1 !== "object" ||
+//     typeof obj2 !== "object" ||
+//     obj1 === null ||
+//     obj2 === null
+//   )
+//     return false;
 
-    this.radius = r;
-  },
-  findArea() {
-    yuza = this.radius ** 2 * this.PI;
-    console.log("Doira yuzasi = ", yuza);
-  },
-};
+//   let keys1 = Object.keys(obj1);
+//   let keys2 = Object.keys(obj2);
 
-doira.addRadius = 5;
-doira.findArea();
+//   if (keys1.length !== keys2.length) return false;
+
+//   for (let key of keys1) {
+//     if (!deepEqual(obj1[key], obj2[key])) return false;
+//   }
+
+//   return true;
+// }
+
+// const obj1 = { a: 1, b: { c: 2, d: 3 } };
+// const obj2 = { a: 1, b: { c: 2, d: 3 } };
+// const obj3 = { a: 1, b: { c: 2, d: 4 } };
+
+// console.log(deepEqual(obj1, obj2));
+// console.log(deepEqual(obj1, obj3));
